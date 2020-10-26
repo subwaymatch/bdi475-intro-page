8cb07e0e14a8f8cd4a54556872805dc66d9b12cd
Ye Joo Park <subwaymatch@gmail.com>
2020-10-26 12:38:48 -0500

    update favicon with netural colors

--- pages/index.tsx
+++ pages/index.tsx
@@ -26,6 +26,28 @@ export default function Home() {
         <title>
           BDI475 Introduction to Data Analytics Applications in Business
         </title>
+
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#a9b7bf" />
+        <meta name="msapplication-TileColor" content="#505d64" />
+        <meta name="theme-color" content="#505d64"></meta>
       </Head>
 
       <div className={styles.sideFloatingBar}>
